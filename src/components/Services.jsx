55e4b0d24875349d3161dab6ce56a1f264480352
Bhattiaman <amanbhatti105@gmail.com>
2025-03-17 21:27:14 +0530

    work

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,71 +1,33 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { AiFillIeCircle, AiFillAndroid, AiFillWindows, AiFillCiCircle } from "react-icons/ai";
 
 const Services = () => {
-  const animations = {
-    whileInView: {
-      x: 0,
-      y: 0,
-      opacity: 1,
-    },
-    one: {
-      opacity: 0,
-      x: "-100%",
-    },
-    twoAndThree: {
-      opacity: 0,
-      y: "-100%",
-    },
-
-    four: {
-      opacity: 0,
-      x: "100%",
-    },
-  };
-
   return (
     <div id="services">
-      <h2>Services</h2>
-      <section>
-        <motion.div
-          className="serviceBox1"
-          whileInView={animations.whileInView}
-          initial={animations.one}
-        >
-          <h3>4+</h3>
-          <p>Month Internship</p>
-        </motion.div>
-        <motion.div
-          className="serviceBox2"
-          whileInView={animations.whileInView}
-          initial={animations.twoAndThree}
-        >
-          <AiFillIeCircle />
-          <span>Web Development Intern</span>
-          <h2>Raise Digital</h2>
-        </motion.div>
-        <motion.div
-          className="serviceBox3"
-          whileInView={animations.whileInView}
-          initial={animations.twoAndThree}
-          transition={{
-            delay: 0.2,
-          }}
-        >
-          <AiFillIeCircle />
-          <span>Web Development Intern</span>
-          <h2>LetsGrowMore</h2>
-        </motion.div>
-        {/* <motion.div
-          className="serviceBox4"
-          whileInView={animations.whileInView}
-          initial={animations.four}
-        >
-          <AiFillWindows />
-          <span>Frontend Development</span>
-        </motion.div> */}
-      </section>
+      <h2>Work Process</h2>
+      <h3>Navigating The Law: Your Assurance Of Peace</h3>
+
+      <div className="process">
+        <div className="step">
+          <h4>Initial Consultation</h4>
+          <p>
+            Our experienced lawyers thoroughly analyze the facts of each case. They then apply the relevant laws to provide clear and actionable advice.
+          </p>
+        </div>
+
+        <div className="step">
+          <h4>Case Evaluation</h4>
+          <p>
+            We prioritize understanding your concerns and aligning with your goals. Your satisfaction is our top priority as we assess the strengths and challenges of your case.
+          </p>
+        </div>
+
+        <div className="step">
+          <h4>Legal Strategy</h4>
+          <p>
+            We develop a customized plan tailored to your unique situation. Our strategy is designed to protect your rights and achieve the best possible outcome.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
